c0c68400f87055206043487a0ddd0b55e35ef87c
basmamouaddine <77249195+basmamouaddine@users.noreply.github.com>
2021-04-25 15:39:14 +0000

    Update blockchain_create.c

--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -1,50 +1,55 @@
 #include "blockchain.h"
-#define GENESIS_TIMESTAMP 1537578000
-#define GENESIS_DATA "Holberton School"
-#define GENESIS_DATA_LEN 16
-#define GENESIS_HASH "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03"
 
 /**
-*blockchain_create - Create a blockchain
-*The Blockchain must contain one block upon creation.
-*This block is called the genesis Block. Its content is static
-*Return: blockchain struct
+* create_genesis_block - creates genesis block
+* Return: a genesis block or NULL if failed
+*/
+block_t *create_genesis_block(void)
+{
+	block_t *genesis;
+
+	genesis = calloc(1, sizeof(*genesis));
+	if (!genesis)
+		return (NULL);
+
+	memcpy(genesis->data.buffer, GENESIS_DATA, GENESIS_DATA_LEN);
+	genesis->data.len = GENESIS_DATA_LEN;
+	*(genesis->data.buffer + GENESIS_DATA_LEN) = '\0';
+
+	memcpy(genesis->hash, GENESIS_HASH, SHA256_DIGEST_LENGTH);
+	genesis->info.timestamp = GENESIS_TIMESTAMP;
+	return (genesis);
+}
+
+/**
+* blockchain_create - creates a blockchain (Genesis Block)
+* Return: a new blockchain or NULL if failed
 */
 blockchain_t *blockchain_create(void)
 {
-	blockchain_t *list;
+	blockchain_t *blockchain;
 	block_t *genesis;
 
-	list = calloc(1, sizeof(*list));
-	if (list == NULL)
+	blockchain = malloc(sizeof(*blockchain));
+	if (!blockchain)
 		return (NULL);
-	genesis = calloc(1, sizeof(*genesis));
-	if (genesis == NULL)
+	genesis = create_genesis_block();
+	if (!genesis)
 	{
-		free(list);
+		free(blockchain);
 		return (NULL);
 	}
-	list->chain = llist_create(MT_SUPPORT_TRUE);
-	if (list->chain == NULL)
+	blockchain->chain = llist_create(MT_SUPPORT_TRUE);
+	if (!blockchain->chain)
 	{
-		free(list);
-		free(genesis);
+		free(blockchain), free(genesis);
 		return (NULL);
 	}
-	genesis->info.index = 0;
-	genesis->info.difficulty = 0;
-	genesis->info.timestamp = GENESIS_TIMESTAMP;
-	genesis->info.nonce = 0;
-	memset(genesis->info.prev_hash, 0, SHA256_DIGEST_LENGTH);
-	memcpy(&(genesis->data.buffer), GENESIS_DATA, GENESIS_DATA_LEN);
-	genesis->data.len = GENESIS_DATA_LEN;
-	memcpy(&(genesis->hash), GENESIS_HASH, SHA256_DIGEST_LENGTH);
-	if (llist_add_node(list->chain, genesis, ADD_NODE_FRONT) == -1)
+	if (llist_add_node(blockchain->chain, genesis, ADD_NODE_FRONT) == -1)
 	{
-		free(genesis);
-		llist_destroy(list->chain, 0, NULL);
-		free(list);
+		llist_destroy(blockchain->chain, 1, NULL);
+		free(blockchain), free(genesis);
 		return (NULL);
 	}
-	return (list);
+	return (blockchain);
 }
