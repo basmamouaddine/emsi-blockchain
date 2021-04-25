df742fecd43380ca6aaa8f79d92967faba1860f4
basmamouaddine <77249195+basmamouaddine@users.noreply.github.com>
2021-04-25 15:36:27 +0000

    Update blockchain_create.c

--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -1,29 +1,50 @@
 #include "blockchain.h"
-blockchain_t *blockchain_create(void){
-int i=0;
-char *st =  "Holberton School";
-char *blk_hash = "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03";
-block_t node;
-block_data_t dataNode;
-block_info_t infoNode;
-blockchain_t *blockchain;
-blockchain = malloc(sizeof(*blockchain));
-blockchain->chain = llist_create(MT_SUPPORT_TRUE);
-infoNode.index = 0;
-infoNode.difficulty = 0;
-infoNode.timestamp = 1537578000;
-infoNode.nonce = 0;
-for(i=0; i <= 32 ; i++)
-{
-	infoNode.prev_hash[i] = (uint8_t) 0;
-	node.hash[i] = (uint8_t)blk_hash[i];
-}
+#define GENESIS_TIMESTAMP 1537578000
+#define GENESIS_DATA "Holberton School"
+#define GENESIS_DATA_LEN 16
+#define GENESIS_HASH "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03"
 
-for(i=0; i <= 16 ; i++)
-	dataNode.buffer[i] = (int8_t) st[i];
-dataNode.len = 16;
-node.info = infoNode;
-node.data = dataNode;
-llist_add_node(blockchain->chain,(llist_node_t *)& node,ADD_NODE_FRONT);
-return(blockchain);
+/**
+*blockchain_create - Create a blockchain
+*The Blockchain must contain one block upon creation.
+*This block is called the genesis Block. Its content is static
+*Return: blockchain struct
+*/
+blockchain_t *blockchain_create(void)
+{
+	blockchain_t *list;
+	block_t *genesis;
+
+	list = calloc(1, sizeof(*list));
+	if (list == NULL)
+		return (NULL);
+	genesis = calloc(1, sizeof(*genesis));
+	if (genesis == NULL)
+	{
+		free(list);
+		return (NULL);
+	}
+	list->chain = llist_create(MT_SUPPORT_TRUE);
+	if (list->chain == NULL)
+	{
+		free(list);
+		free(genesis);
+		return (NULL);
+	}
+	genesis->info.index = 0;
+	genesis->info.difficulty = 0;
+	genesis->info.timestamp = GENESIS_TIMESTAMP;
+	genesis->info.nonce = 0;
+	memset(genesis->info.prev_hash, 0, SHA256_DIGEST_LENGTH);
+	memcpy(&(genesis->data.buffer), GENESIS_DATA, GENESIS_DATA_LEN);
+	genesis->data.len = GENESIS_DATA_LEN;
+	memcpy(&(genesis->hash), GENESIS_HASH, SHA256_DIGEST_LENGTH);
+	if (llist_add_node(list->chain, genesis, ADD_NODE_FRONT) == -1)
+	{
+		free(genesis);
+		llist_destroy(list->chain, 0, NULL);
+		free(list);
+		return (NULL);
+	}
+	return (list);
 }
