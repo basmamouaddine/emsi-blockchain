f84e7a2f917de43bd8772aa321912564058faeb5
basmamouaddine <77249195+basmamouaddine@users.noreply.github.com>
2021-04-28 15:26:10 +0000

    Update block_hash.c

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -1,15 +1,17 @@
 #include "blockchain.h"
-/**
-* block_hash - get the hash of a block
-* @block: block to be hashed
-* @hash_buf: buffer for digest
-* Return: hash digest
-*/
-uint8_t *block_hash(block_t const *block,
-uint8_t hash_buf[SHA256_DIGEST_LENGTH])
-{
-size_t len;
 
-len = sizeof(block->info) + block->data.len;
-return (sha256((int8_t const *)block, len, hash_buf));
+/**
+ * block_hash - Computes the hash of a Block
+ * @block: pointer to block to hash
+ * @hash_buf: buffer to store hash
+ *
+ * Return: pointer to @hash_buf
+ */
+uint8_t *block_hash(block_t const *block,
+		    uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+{
+	size_t len = sizeof(block->info) + block->data.len;
+
+	sha256((void *)&(block->info), len, hash_buf);
+	return (hash_buf);
 }
